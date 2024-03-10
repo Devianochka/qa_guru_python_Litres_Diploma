fa2c1d24e86770ffd68af4c095998d5844a805d0
Diana Sagaeva <d_sagaeva@mail.ru>
2024-03-10 03:57:03 +0300

    commit

--- litres_project/pages/mobile/main_page.py
+++ litres_project/pages/mobile/main_page.py
@@ -6,14 +6,14 @@ class AndroidMainPage:
 
     def selecting_application_language(self):
 
-        if browser.element((AppiumBy.ID, "ru.litres.android:id/choosebutton")).matching(be.clickable):
-            browser.element((AppiumBy.ID, "ru.litres.android:id/choosebutton")).click()
+        if browser.element((AppiumBy.ID, "ru.litres.android.global:id/choosebutton")).matching(be.clickable):
+            browser.element((AppiumBy.ID, "ru.litres.android.global:id/choosebutton")).click()
         return self
 
     def hiding_adult_content(self):
 
-        if browser.element((AppiumBy.ID, "ru.litres.android:id/btnDisableAdultContent")).click():
-            browser.element((AppiumBy.ID, "ru.litres.android:id/btnConfirmDisableAdultContent")).click()
+        if browser.element((AppiumBy.ID, "ru.litres.android.global:id/btnDisableAdultContent")).click():
+            browser.element((AppiumBy.ID, "ru.litres.android.global:id/btnConfirmDisableAdultContent")).click()
         return self
 
 

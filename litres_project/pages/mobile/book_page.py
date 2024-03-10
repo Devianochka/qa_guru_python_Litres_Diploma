a4046c9680323192c01292da580d4ba3319545df
Diana Sagaeva <d_sagaeva@mail.ru>
2024-03-10 04:05:30 +0300

    commit

--- litres_project/pages/mobile/book_page.py
+++ litres_project/pages/mobile/book_page.py
@@ -5,28 +5,28 @@ from appium.webdriver.common.appiumby import AppiumBy
 class AndroidBookPage:
 
     def adding_book_to_saved(self):
-        browser.element((AppiumBy.ID, "ru.litres.android:id/bookPostpone")).click()
+        browser.element((AppiumBy.ID, "ru.litres.android.global:id/bookPostpone")).click()
         return self
 
     def go_to_saved_tab(self):
-        browser.element((AppiumBy.ID, "ru.litres.android:id/nav_my_audiobooks")).click()
+        browser.element((AppiumBy.ID, "ru.litres.android.global:id/nav_my_audiobooks")).click()
         browser.element((AppiumBy.ACCESSIBILITY_ID, "SAVED")).click()
         return self
 
     def book_must_be_added_to_saved(self):
-        (browser.element((AppiumBy.ID, "ru.litres.android:id/bookName"))
+        (browser.element((AppiumBy.ID, "ru.litres.android.global:id/bookName"))
          .should(have.text("The Adventures of Tom Sawyer")))
         return self
 
     def removing_book_from_saved(self):
-        browser.element((AppiumBy.ID, "ru.litres.android:id/bookName")).click()
-        browser.element((AppiumBy.ID, "ru.litres.android:id/bookPostpone")).click()
+        browser.element((AppiumBy.ID, "ru.litres.android.global:id/bookName")).click()
+        browser.element((AppiumBy.ID, "ru.litres.android.global:id/bookPostpone")).click()
         return self
 
     def book_must_be_removed_from_saved(self):
-        results = browser.all((AppiumBy.ID, 'ru.litres.android:id/empty_view'))
+        results = browser.all((AppiumBy.ID, 'ru.litres.android.global:id/empty_view'))
         results.should(have.size(1))
-        browser.element((AppiumBy.ID, 'ru.litres.android:id/action_button')).should(be.visible)
+        browser.element((AppiumBy.ID, 'ru.litres.android.global:id/action_button')).should(be.visible)
         return self
 
 

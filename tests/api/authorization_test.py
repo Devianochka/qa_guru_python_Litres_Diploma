933a454632319135b9ed5b30301b2a4a98fbb230
Diana Sagaeva <d_sagaeva@mail.ru>
2024-03-11 09:53:40 +0300

    commit

--- tests/api/authorization_test.py
+++ tests/api/authorization_test.py
@@ -1,9 +1,8 @@
-import os
 import allure
 import jsonschema
 from litres_project.schema.load_schema import load_schema
 from litres_project.utils.api_requests import api_post
-from tests.api.conftest import url, user_email, user_password, invalid_password, headers
+from tests.api.conftest import url, email, password, invalid_password, headers
 
 
 @allure.epic('API. Authorized')
@@ -16,8 +15,6 @@ from tests.api.conftest import url, user_email, user_password, invalid_password,
 def test_authorization_registered_user():
     schema = load_schema('authorization.json')
     url = "/auth/login"
-    email = os.getenv('USER_EMAIL')
-    password = os.getenv('USER_PASSWORD')
 
     result = api_post(url, headers=headers, json={"login": email, "password": password})
 
@@ -36,8 +33,7 @@ def test_authorization_registered_user():
 def test_authorization_unregistered_user():
     schema = load_schema('failed_authorization.json')
     url = "/auth/login"
-    email = os.getenv('USER_EMAIL')
-    invalid_password = os.getenv('UNREGISTERED_USER_PASSWORD')
+
 
     result = api_post(url, headers=headers, json={"login": email, "password": invalid_password})
 
